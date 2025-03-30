35cbf4160cbd3ec19ef3f0256454712e3852eb85
danielaj2004 <danielajoya12@gmail.com>
2025-03-30 19:11:56 -0400

    Display products in the DOM with name, price, and image

--- cc_16.js
+++ cc_16.js
@@ -25,3 +25,34 @@ async function fetchProductsAsync() {
           handleError(error); // catching error of products
         }
 }
+
+// Task 4 Display the Products
+function displayProducts(products) {
+    const container = document.getElementById('product-container');
+    container.innerHTML = ''; // Clears prior content
+  
+    products.slice(0, 5).forEach((product) => {
+      const { name, price, image } = product.fields; // product fields
+  
+      const productCards = document.createElement('div'); // Creating the product Cards
+      productCards.setAttribute('class','product-card');
+  
+      const productName = document.createElement('h2'); // creating product name
+      productName.setAttribute('class', 'product-name');
+      productName.textContent = product.fields.name;
+  
+      const productPrice = document.createElement('p'); // creating product price
+      productPrice.setAttribute('class', 'product-price' );
+      productPrice.textContent = `$${(price/100).toFixed(2)}`;
+  
+      const productImage = document.createElement('img'); // creating product image
+      productImage.src = image[0].url;
+      productImage.alt = name;
+  
+      productCards.appendChild(productName); // appending product name
+      productCards.appendChild(productPrice); // appending price
+      productCards.appendChild(productImage); // // appending image
+
+      container.appendChild(productCards); // appending product cards to a container
+    });
+  }
